eeb5eaca142fa7fe034481c6310510a8b11c007e
perci <ploomutoo@gmail.com>
2024-07-26 00:53:33 -0400

    merge

--- crawl-ref/source/book-data.h
+++ crawl-ref/source/book-data.h
@@ -62,7 +62,6 @@ static const vector<spell_type> spellbook_templates[] =
 },
 
 {   // Book of Summonings
-    SPELL_SUMMON_DEMON,
     SPELL_SUMMON_FOREST,
     SPELL_SUMMON_MANA_VIPER,
     SPELL_SUMMON_CACTUS,
@@ -281,7 +280,6 @@ static const vector<spell_type> spellbook_templates[] =
 },
 
 {   // Book of Beasts
-    SPELL_SUMMON_DEMON,
     SPELL_SUMMON_ICE_BEAST,
     SPELL_SUMMON_MANA_VIPER,
     SPELL_MONSTROUS_MENAGERIE,
